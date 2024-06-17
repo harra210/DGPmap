ba838090ff9dece193cf799d1bb4c4dff20df0e4
Alex Harris <harra210@gmail.com>
2024-06-17 09:19:09 -0400

    Update DGPmap.sh
    Placed coding for flags and updated subscripts

--- DGPmap.sh
+++ DGPmap.sh
@@ -4,10 +4,76 @@
 # swarmfiles to submit to the cluster. This script will also call additional scripts as needed and be more in line
 # with the original Dog10K pipeline.
 #
-# Script will have section which can have flags added in to allow for the ability to use different alignments.
-# Would be done at a later date.
+# Flag Processing Section to parse flags from the invokation of pipeline and export flags variables to their respective scripts.
 #
-#set -e
+# Currently only have one flag of use to be used in generating proper tables but this function will allow for modification later if different alignments are desired to be used. If that becomes the case, then that will necessitate the modification of subscripts.
+#
+# Fastq defaults
+FQ_IN="NISC"
+#
+# getops string
+opts="f:"
+#
+# Gets the command name without path
+cmd(){ echo `basename $0`; }
+#
+# Help command output
+usage(){
+        echo "\
+                `cmd` [OPTION...]
+        -f, --fastq; Set input fastq style format (default:$OPT)
+        " | column -t -s ";"
+}
+#
+# Error message
+error(){
+        echo "`cmd`: invalid option -- '$1'";
+        echo "Try '`cmd` -h' for more information.";
+        exit 1;
+}
+#
+# Two passes for the flag parsing. The first pass handles the long options and any short option that is in canonical form.
+# The second pass uses `getopt` to canonicalize any erroneous short options and handle them.
+# This second pass will allow for future expansion of the script.
+#
+for pass in 1 2; do
+        while [ -n "$1" ]; do
+                case $1 in
+                        --) shift; break;;
+                        -*) case $1 in
+                                -f|--fastq)     export FQ_IN=$2; shift;;
+                                --*)    error $1;;
+                                -*)     if [ $pass -eq 1 ]; then ARGS="$ARGS $1";
+                                        else error $1; fi;;
+                                esac;;
+                        *) if [ $pass -eq 1 ]; then ARGS="$ARGS $1";
+                                else error $1; fi;;
+                esac
+                shift
+        done
+        if [ $pass -eq 1 ]; then ARGS=`getopt $opts $ARGS`
+                if [ $? != 0 ]; then usage; exit 2; fi; set -- $ARGS
+        fi
+done
+#
+# Handle positional arguments
+if [ -n "$*" ]; then
+        echo "`cmd`: Extra arguments -- $*"
+        echo "Try '`cmd` -h' for more information."
+        exit 1
+fi
+echo "$FQ_IN"
+#
+# Variable Definition Sections that are able to be exported to subscripts. Note there are additional variables in subscripts but those are unable to be exported due to dynamics.
+#
+CanFam4_Ref="/data/Ostrander/Resources/CanFam4_GSD/BWAMEM2/UU_Cfam_GSD_1.0_ROSY.fa"
+export CanFam4_Ref
+#
+knownsite="/data/Ostrander/Resources/CanFam4_GSD/BWAMEM2/UU_Cfam_GSD_1.0.BQSR.DB.bed"
+export knownsite
+#
+interval_list="/data/Ostrander/Resources/CanFam4_GSD/Intervals/Intervals.list"
+export interval_list
 #
 homedir=$(pwd)
 export homedir
@@ -19,7 +85,84 @@ export tmpdir
 cd $homedir
 #
 # Here, we blank all of the swarmfiles to be used from alignment to completion
-> bwa_to_picard.swarm
+#> bwa_to_picard.swarm
+#> mergeDedup.swarm
+#> bqsr_BaseRecalibrator.swarm
+#> bqsr_gatherBQSRReports.swarm
+#> bqsr_ApplyBQSR.swarm
+#> bqsr_gatherBQSRBams.swarm
+#> haplotypecaller.swarm
+#
+"DGPmap_v1.1.sh" 162L, 4705C                                                                                     47,4-32       Top
+#!/bin/bash
+#
+# This begins after the creation of sample tables and will use the table to parse out all of the
+# swarmfiles to submit to the cluster. This script will also call additional scripts as needed and be more in line
+# with the original Dog10K pipeline.
+#
+# Flag Processing Section to parse flags from the invokation of pipeline and export flags variables to their respective scripts.
+#
+#
+# Fastq defaults
+FQ_IN="NISC"
+#
+# getops string
+opts="f:"
+#
+# Gets the command name without path
+cmd(){ echo `basename $0`; }
+#
+# Help command output
+usage(){
+        echo "\
+                `cmd` [OPTION...]
+        -f, --fastq; Set input fastq style format (default:$OPT)
+        " | column -t -s ";"
+}
+#
+# Error message
+error(){
+        echo "`cmd`: invalid option -- '$1'";
+        echo "Try '`cmd` -h' for more information.";
+        exit 1;
+}
+# The second pass uses `getopt` to canonicalize any erroneous short options and handle them.
+# This second pass will allow for future expansion of the script.
+#
+                case $1 in
+                                -f|--fastq)     export FQ_IN=$2; shift;;
+                                --*)    error $1;;
+                                -*)     if [ $pass -eq 1 ]; then ARGS="$ARGS $1";
+                        *) if [ $pass -eq 1 ]; then ARGS="$ARGS $1";
+                shift
+        done
+        if [ $pass -eq 1 ]; then ARGS=`getopt $opts $ARGS`
+done
+if [ -n "$*" ]; then
+        echo "`cmd`: Extra arguments -- $*"
+        echo "Try '`cmd` -h' for more information."
+echo "$FQ_IN"
+#
+CanFam4_Ref="/data/Ostrander/Resources/CanFam4_GSD/BWAMEM2/UU_Cfam_GSD_1.0_ROSY.fa"
+export CanFam4_Ref
+#
+knownsite="/data/Ostrander/Resources/CanFam4_GSD/BWAMEM2/UU_Cfam_GSD_1.0.BQSR.DB.bed"
+export knownsite
+#
+interval_list="/data/Ostrander/Resources/CanFam4_GSD/Intervals/Intervals.list"
+export interval_list
+#
+homedir=$(pwd)
+export homedir
+#
+cd ../tmp
+tmpdir=$(pwd)
+export tmpdir
+#
+cd $homedir
+#
+# Here, we blank all of the swarmfiles to be used from alignment to completion
+#> bwa_to_picard.swarm
 #> mergeDedup.swarm
 #> bqsr_BaseRecalibrator.swarm
 #> bqsr_gatherBQSRReports.swarm
@@ -58,20 +201,39 @@ cd $FILE_DIR # Change into the parent directory
 #
 # Begin the loop section to work on generating swarmfiles.
 # Firstly, we generate the alignment sample tables
+#
 for dir in ${basedir[@]};
-	do
-		( [ -d "$dir" ] && cd $dir && echo "Entering into $dir and generating alignment tables" && bash "$scriptdir"/table-generation.sh )
+        do
+                ( [ -d "$dir" ] && cd $dir && echo "Entering into $dir and generating alignment tables" && bash "$scriptdir"/table-generation.sh )
 done
 #
 # This part builds the BWAMEM2 alignment swarmfile
-for dir in ${basedir[@]};
-	do
-		( [ -d "$dir" ] && cd $dir && echo "Entering into $dir and generating alignment swarm" && bash "$scriptdir"/process-BWA.sh )
-done
+#
+#for dir in ${basedir[@]};
+#       do
+#               ( [ -d "$dir" ] && cd $dir && echo "Entering into $dir and generating alignment swarm" && bash "$scriptdir"/process-BWA.sh )
+#done
 #
 # This part builds the samtools and GATK MarkDuplicates swarmfile
-for dir in ${basedir[@]};
-        do
-                ( [ -d "$dir" ] && cd $dir && echo "Entering into $dir and generating alignment swarm" && bash "$scriptdir"/process-sortmd.sh )
-done
+#
+#for dir in ${basedir[@]};
+#        do
+#                ( [ -d "$dir" ] && cd $dir && echo "Entering into $dir and generating sorted MarkDuplicates swarm" && bash "$scriptdir"/process-sortmd.sh )
+#done
+#
+# Now build the BQSR Recal table swarmfile
+#
+#for dir in ${basedir[@]};
+#        do
+#                ( [ -d "$dir" ] && cd $dir && echo "Entering into $dir and generating BQSR tables swarm" && bash "$scriptdir"/process-BQSR.sh )
+#done
+#
+# Gather BQSR Reports
+#
+#for dir in ${basedir[@]};
+#        do
+#                ( [ -d "$dir" ] && cd $dir && echo "Entering into $dir and gathering BQSR tables to write to swarm" && bash "$scriptdir"/process-BQSRreportsGather.sh )
+#done
+#
+# Apply BaseRecalibration by chromosome
 #
