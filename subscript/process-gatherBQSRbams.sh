d6127759aa4cc92d6042f5cc0697841f96dd9f05
Alex Harris <harra210@gmail.com>
2024-06-17 09:31:58 -0400

    Add files via upload

---
+++ subscript/process-gatherBQSRbams.sh
@@ -0,0 +1,46 @@
+#!/bin/bash
+#
+# This is an internal script for DGPmap to gather bam files previously created per chromosome
+#
+FQ_DIR=$(pwd)
+#
+## Variable Definition Section
+#
+cd txt_files
+TXT_DIR=$(pwd)
+> bqsr_gatherfiles.tmp
+cd $FQ_DIR
+#
+cd BQSR_chr
+BQSRchr_DIR=$(pwd)
+cd $FQ_DIR
+#
+cd BQSR
+BQSR_DIR=$(pwd)
+cd $FQ_DIR
+#
+PREFIX="-I "
+##
+#
+FN=( $(awk 'NR==1{print$1; exit}' "$TXT_DIR"/final_header.txt ) )
+#
+# Generate BQSR sample name list into a tmp file in the text files folder
+#
+while read g
+do
+	echo ""${FN[0]}"."$g".BQSR.bam" >> "$TXT_DIR"/bqsr_gatherfiles.tmp
+done < "$interval_list"
+#
+# Now for chr Y and the unmapped reads
+echo ""${FN[0]}".chrY.BQSR.bam" >> "$TXT_DIR"/bqsr_gatherfiles.tmp
+#
+# Import the chromosomal bam names into an array
+#
+IFS=,$'\n' read -d '' -r -a bqsrbamfiles < "$TXT_DIR"/bqsr_gatherfiles.tmp
+declare -a bqsrbamfiles
+#
+# Now to fully generate the swarmfile
+#
+echo "cd "$BQSRchr_DIR"; gatk --java-options \"-Xmx6G\" GatherBamFiles "${bqsrbamfiles[*]/#/$PREFIX}" --output "$BQSR_DIR"/"${FN[0]}".BQSR.bam" >> "$homedir"/bqsr_gatherBQSRBams.swarm
+#
+## This completes collection of BQSR bam files
---
+++ subscript/process-haplotypecaller.sh
@@ -0,0 +1,35 @@
+#!/bin/bash
+#
+# This is an internal script for DGPmap to generate gVCFs per chromosome
+#
+## Variable Definition Section
+#
+FQ_DIR=$(pwd)
+#
+cd txt_files
+TXT_DIR=$(pwd)
+cd $FQ_DIR
+#
+cd BQSR
+BQSR_DIR=$(pwd)
+cd $FQ_DIR
+#
+cd gVCF/HC
+HC_DIR=$(pwd)
+cd $FQ_DIR
+#
+## End Variable Definitions
+#
+# Generate the sample name via awk from the original sample table
+FN=( $(awk 'NR==1{print $1; exit}' "$TXT_DIR"/final_header.txt ) )
+#
+# Now to generate the swarm file. For chr1 - chrX use a while loop and the for the remainder a simple echo line.
+#
+while read g
+do
+	echo "cd "$BQSR_DIR"; gatk -- java-options \"-Xmx4G\" HaplotypeCaller -R "$CanFam4_Ref" --tmp-dir /lscratch/\$SLURM_JOB_ID -I "${FN[0]}".BQSR.bam -L "$g" -O "$HC_DIR"/"${FN[0]}"_"$g".g.vcf.gz -ERC GVCF" >> "$homedir"/haplotypecaller.swarm
+done < "$interval_list"
+#
+echo "cd "$BQSR_DIR"; gatk -- java-options \"-Xmx4G\" HaplotypeCaller -R "$CanFam4_Ref" --tmp-dir /lscratch/\$SLURM_JOB_ID -I "${FN[0]}".BQSR.bam -XL "$interval_list" -O "$HC_DIR"/"${FN[0]}"_chrY.g.vcf.gz -ERC GVCF" >> "$homedir"/haplotypecaller.swarm
+#
+## This completes the HaplotypeCaller script
