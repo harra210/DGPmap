a38823a041090c5c57743c3b08f810e019f03b2b
Alex Harris <harra210@gmail.com>
2024-07-09 08:30:37 -0800

    Modified file via upload
    Fixed bug where output flag is incorrectly applied

--- subscript/process-gatherBQSRbams.sh
+++ subscript/process-gatherBQSRbams.sh
@@ -24,50 +24,50 @@ PREFIX="-I "
 ## For NISC-style FastQ inputs
 #
 if [ "$FQ_IN" = "NISC" ];
-        then
-        FN=( $(awk 'NR==1{print$1; exit}' "$TXT_DIR"/final_header.txt ) )
+	then
+	FN=( $(awk 'NR==1{print$1; exit}' "$TXT_DIR"/final_header.txt ) )
 #
 # Generate BQSR sample name list into a tmp file in the text files folder
 #
-        while read g
-                do
-                        echo ""${FN[0]}"."$g".BQSR.bam" >> "$TXT_DIR"/bqsr_gatherfiles.tmp
-        done < "$interval_list"
+	while read g
+		do
+			echo ""${FN[0]}"."$g".BQSR.bam" >> "$TXT_DIR"/bqsr_gatherfiles.tmp
+	done < "$interval_list"
 #
 # Now for chr Y and the unmapped reads
-        echo ""${FN[0]}".chrY.BQSR.bam" >> "$TXT_DIR"/bqsr_gatherfiles.tmp
+	echo ""${FN[0]}".chrY.BQSR.bam" >> "$TXT_DIR"/bqsr_gatherfiles.tmp
 #
 # Import the chromosomal bam names into an array
 #
-        IFS=,$'\n' read -d '' -r -a bqsrbamfiles < "$TXT_DIR"/bqsr_gatherfiles.tmp
-        declare -a bqsrbamfiles
+	IFS=,$'\n' read -d '' -r -a bqsrbamfiles < "$TXT_DIR"/bqsr_gatherfiles.tmp
+	declare -a bqsrbamfiles
 #
 # Now to fully generate the swarmfile
 #
-        echo "cd "$BQSRchr_DIR"; gatk --java-options \"-Xmx6G\" GatherBamFiles "${bqsrbamfiles[*]/#/$PREFIX}" --output "$BQSR_DIR"/"${FN[0]}".BQSR.bam" >> "$homedir"/bqsr_gatherBQSRBams.swarm
+	echo "cd "$BQSRchr_DIR"; gatk --java-options \"-Xmx6G\" GatherBamFiles "${bqsrbamfiles[*]/#/$PREFIX}" -O "$BQSR_DIR"/"${FN[0]}".BQSR.bam" >> "$homedir"/bqsr_gatherBQSRBams.swarm
 #
 ## For SRA-style FastQ inputs
 #
 elif [ "$FQ_IN" = "SRA" ];
-        then
-        FN=( $(awk 'NR==1{print$1; exit}' "$FQ_DIR"/*.runs.table ) )
+	then
+ 	FN=( $(awk 'NR==1{print$1; exit}' "$FQ_DIR"/*.runs.table ) )
   #
-        while read g
-                do
-                        echo ""${FN[0]}"."$g".BQSR.bam" >> "$TXT_DIR"/bqsr_gatherfiles.tmp
-        done < "$interval_list"
+  	while read g
+		do
+			echo ""${FN[0]}"."$g".BQSR.bam" >> "$TXT_DIR"/bqsr_gatherfiles.tmp
+	done < "$interval_list"
 #
 # Now for chr Y and the unmapped reads
-        echo ""${FN[0]}".chrY.BQSR.bam" >> "$TXT_DIR"/bqsr_gatherfiles.tmp
+	echo ""${FN[0]}".chrY.BQSR.bam" >> "$TXT_DIR"/bqsr_gatherfiles.tmp
 #
 # Import the chromosomal bam names into an array
 #
-        IFS=,$'\n' read -d '' -r -a bqsrbamfiles < "$TXT_DIR"/bqsr_gatherfiles.tmp
-        declare -a bqsrbamfiles
+	IFS=,$'\n' read -d '' -r -a bqsrbamfiles < "$TXT_DIR"/bqsr_gatherfiles.tmp
+	declare -a bqsrbamfiles
 #
 # Now to fully generate the swarmfile
 #
-        echo "cd "$BQSRchr_DIR"; gatk --java-options \"-Xmx6G\" GatherBamFiles "${bqsrbamfiles[*]/#/$PREFIX}" --output "$BQSR_DIR"/"${FN[0]}".BQSR.bam" >> "$homedir"/bqsr_gatherBQSRBams.swarm
+	echo "cd "$BQSRchr_DIR"; gatk --java-options \"-Xmx6G\" GatherBamFiles "${bqsrbamfiles[*]/#/$PREFIX}" -O "$BQSR_DIR"/"${FN[0]}".BQSR.bam" >> "$homedir"/bqsr_gatherBQSRBams.swarm
 else
-        exit 1
+	exit 1
 fi
